8c00bd4ae92f16ab8c4c3666e3ff9137cbe2911b
mahendrakr <mahendrakumar7028@gmail.com>
2021-07-14 07:45:55 +0530

    eclipse shortcut and some updates COMMITED BY AADI

--- java11/src/java11/Animal.java
+++ java11/src/java11/Animal.java
@@ -3,15 +3,16 @@ package java11;
 public class Animal {
 int tail = 2 ;
 int ears = 1 ;
-void nature() {
+void getNature() {
 	System.out.println("Animals are innocent");
 }
-void food() {
+void eatFood() {
 	System.out.println("some animals eat both herbs and meat where some eat either meat or herbs ");
 }
 public static void main(String[] args) {
 	Animal a  = new Animal();
 	System.out.println(a.ears);
-	a.food();
+	a.eatFood();
+	System.out.println(a.tail);
 }
 }
--- java11/src/java11/Student.java
+++ java11/src/java11/Student.java
@@ -3,15 +3,15 @@ package java11;
 public class Student {
 int hands = 2 ;
 int brain = 1 ;
-void dress() {
+void getDress() {
 	System.out.println("students dress tidy");
 }
-void nature() {
+void getNature() {
 	System.out.println("hard working");
 }
 public static void main(String[] args) {
 	Student s = new Student ();
 	System.out.println(s.brain);
-	s.nature();
+	s.getNature();
 }
 }
