7dcc0b970494388de489757ab9f018133ae26aee
mahendrakr <mahendrakumar7028@gmail.com>
2021-07-29 09:29:03 +0530

    29 july 2021 committed by shreya

--- java11/src/java11/Test.java
+++ java11/src/java11/Test.java
@@ -2,7 +2,10 @@ package java11;
 import book.Book;
 import book.A;
 import book.B;
-//import book.*;
+import book.*;
+import book.author.Author;
+import static book.author.Author.m1;
+import static book.author.Author.name;
 public class Test {
 	
 
@@ -13,7 +16,13 @@ public static void main(String[] args) {
 	b.m1();
 	A a = new A();
 	B b1 = new B();
-	
+	Author au = new Author();
+	book.author.Author.m1();
+	m1();
+	String name2 = book.author.Author.name;
+	String name1 = name;
+	java.lang.System.out.println("hello aadi");
+//	System.out.println("hello aadi");
 }
 }
    
