cdf9d867f8989699be777e43d9f3b5cd06566522
mahendrakr <mahendrakumar7028@gmail.com>
2021-08-19 21:02:57 +0530

    LECTURE 19AUG2021_BITWISE AND SHORTCIRCUIT_IMPLICIT&EXPLICIT TYPECASTING_COMMITTED BY AADI

---
+++ java11/src/operators/TildOperators.java
@@ -0,0 +1,9 @@
+package operators;
+
+public class TildOperators {
+	public static void main(String[] args) {
+		System.out.println(~-6);
+		System.out.println(!true);
+		System.out.println(!(4>2));
+	}
+}
---
+++ java11/src/operators/TypeCasting.java
@@ -0,0 +1,11 @@
+package operators;
+//HW : convert 140 int to byte
+public class TypeCasting {
+public static void main(String[] args) {
+	byte b = 10;
+	int i = b;
+	int x = 140;
+	byte y = (byte)x;
+	System.out.println(y);
+}
+}
