d8171cfb4c1a738c48428de672963d7b2d04524a
mahendrakr <mahendrakumar7028@gmail.com>
2021-08-30 20:36:41 +0530

    lecture30aug21_itterative statement COMMITTED BY AADI

---
+++ java11/src/controlFlow/ItterativeStatement.java
@@ -0,0 +1,27 @@
+package controlFlow;
+//write a Program of factorial
+public class ItterativeStatement {
+	public void printTable(int n) {
+		for(int i = 1;i<=10;i++) {
+			System.out.println(n*i);
+		}
+	}
+	public void printSum(int n) {
+		int r = n%10;
+		int p = n/10;
+		System.out.println(r + " " + p);
+	}
+	 public static void main(String[] args) {
+		//for(int i= 0;i<10;i++) {
+			//System.out.println("aadi");
+		//}
+		 ItterativeStatement is = new ItterativeStatement();
+		 //is.printTable(5);
+		 is.printSum(12345);
+		 int i = 1;
+		 while(i<10) {
+			 System.out.println("aadi");
+			 i++;
+		 }
+	}
+}
